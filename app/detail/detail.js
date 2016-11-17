8271ea58ec303a81ecf4d7d1f6e477ebc10f97cf
徐冠丰 <18657604670@163.com>
2016-11-18 07:18:28 +0900

    up

--- app/detail/detail.js
+++ app/detail/detail.js
@@ -1,8 +1,22 @@
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-route.d.ts" />
+interface IDetailScope extends ng.IScope {
+    chartConfig: any;
+    fund: Array<any>;
+    priceDate: Date;
+}
+interface IDetailRouteParamService extends ng.route.IRouteParamsService {
+    id: string;
+}
+interface IResponse extends ng.IHttpResponseTransformer {
+    data: any;
+}
+
 'use strict';
 
 angular.module('myApp.detail', ['ngRoute', 'highcharts-ng'])
 
-    .controller('detailCtrl', function DetailCtrl($scope, $http, $routeParams) {
+    .controller('detailCtrl', function ($scope: IDetailScope, $http: ng.IHttpService, $routeParams: IDetailRouteParamService) {
 
 
 
@@ -12,43 +26,79 @@ angular.module('myApp.detail', ['ngRoute', 'highcharts-ng'])
             options: {
                 //This is the Main Highcharts chart config. Any Highchart options are valid here.
                 //will be overriden by values specified below.
-                chart: {
-                    type: 'bar'
-                    // type: 'candlestick'
+                rangeSelector: {
+                    selected: 1
                 },
-                tooltip: {
-                    style: {
-                        padding: 10,
-                        fontWeight: 'bold'
+
+                title: {
+                    text: 'AAPL Stock Price'
+                },
+                navigator: {
+                    enabled:true,
+                    height: 40,
+                    margin: 25,
+                    series: {
+                        type: 'areaspline',
+                        color: '#4572A7',
+                        fillOpacity: 0.05,
+                        dataGrouping: {
+                            smoothed: true
+                        },
+                        lineWidth: 1,
+                        marker: {
+                            enabled: false
+                        }
                     }
+                },
+                scrollbar:{
+                    enabled:false
                 }
+
+
             },
             //The below properties are watched separately for changes.
 
             //Series object (optional) - a list of series using normal Highcharts series options.
+            // series: [{
+            //     data: [10, 15, 12, 8, 7]
+            // }],
             series: [{
-                data: [10, 15, 12, 8, 7]
+                type: 'candlestick',
+                showInNavigator: true,
+                name: 'AAPL Stock Price',
+                data: [],
+                dataGrouping: {
+                    units: [
+                        [
+                            'week', // unit name
+                            [1] // allowed multiples
+                        ], [
+                            'month',
+                            [1, 2, 3, 4, 6]
+                        ]
+                    ]
+                }
             }],
             //Title configuration (optional)
-            title: {
-                text: 'Hello'
-            },
+            // title: {
+            //     text: 'Hello'
+            // },
             //Boolean to control showing loading status on chart (optional)
             //Could be a string if you want to show specific loading text.
             loading: false,
             //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
             //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
-            xAxis: {
-                currentMin: 0,
-                currentMax: 20,
-                title: {text: 'values'}
-            },
+            // xAxis: {
+            //     currentMin: 0,
+            //     currentMax: 20,
+            //     title: { text: 'values' }
+            // },
             //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
             useHighStocks: true,
             //size (optional) if left out the chart will default to size of the div or something sensible.
             size: {
-                width: 400,
-                height: 300
+                width: 514,
+                height: 310
             },
             //function (optional)
             func: function (chart) {
@@ -57,9 +107,14 @@ angular.module('myApp.detail', ['ngRoute', 'highcharts-ng'])
         };
 
         //jsonからデータを動的に読み込む
-        $http.get('json/' + $routeParams.id + '.json').then(function (response) {
+        $http.get('json/' + $routeParams.id + '.json').then((response: IResponse) => {
             $scope.fund = response.data;
         });
+
+        $http.jsonp('https://www.highcharts.com/samples/data/jsonp.php?a=e&filename=aapl-ohlc.json&callback=JSON_CALLBACK')
+            .success(function (res) {
+                $scope.chartConfig.series[0].data = res;
+            });
 
     });
 
